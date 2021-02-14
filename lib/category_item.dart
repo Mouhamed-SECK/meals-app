ba4cd74a9d3b60f42313be2930b8fa5241db6bb1
Mouhamed27-99 <medsoseck@gmail.com>
2021-02-14 16:06:58 +0000

    feat : add route from categories screen to category meals screen

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
 
+import 'category_meals_screen.dart';
+
 class CategoryItem extends StatelessWidget {
+  final String id;
   final String title;
   final Color color;
 
-  const CategoryItem(this.title, this.color);
+  const CategoryItem(this.id, this.title, this.color);
+
+  void selectCategory(BuildContext ctx) {
+    Navigator.pushNamed(
+      ctx,
+      CategoryMealsScreen.routeName,
+      arguments: {'id': id, 'title': title},
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
       child: Container(
         padding: const EdgeInsets.all(15),
         child: Text(
